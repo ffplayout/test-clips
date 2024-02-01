3f8633579192c71f279734e5c0c2f8a664a5a42a
jb-alvarado <jb@pixelcrusher.de>
2024-02-01 21:43:24 +0100

    change settings

--- _gen-shorts.py
+++ _gen-shorts.py
@@ -80,7 +80,7 @@ def main():
 
         cmd = [
             'ffmpeg', '-hide_banner', '-f', 'lavfi', '-i',
-            f'color=c={color}:s=1024x576:d={dur}:r=25',
+            f'color=c={color}:s=640x360:d={dur}:r=25',
             '-f', 'lavfi', '-i',
             f'sine=frequency={freq}:duration={dur}',
             '-vf', (
@@ -92,10 +92,9 @@ def main():
                 + ":fontsize=18':boxborderw=6:x=(main_w/2-text_w/2):"
                 "y=(h-line_h)*0.855:box=1:boxcolor=black"),
             '-aspect', '16:9', '-pix_fmt', 'yuv420p',
-            '-t', f'{dur}', '-c:v', 'libx265',  '-preset', 'veryfast',
-            '-tag:v', 'hvc1', '-c:a', 'libfdk_aac',
-            '-profile:a', 'aac_he_v2', '-ac', '2', '-ar', '44100',
-            '-b:a', '16k', f'test_list/{color}_{postfix}.mp4']
+            '-t', f'{dur}', '-c:v', 'libx264',
+            '-c:a', 'libfdk_aac', '-profile:a', 'aac_he_v2', '-ac', '2',
+            '-ar', '44100', '-b:a', '8k', f'test_list/{color}_{postfix}.mp4']
 
         check_output(cmd)
 
